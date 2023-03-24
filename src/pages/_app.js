a38fc71979a274afc4488a47875fdce377891f5e
Neilen <75497135+NeilenC@users.noreply.github.com>
2023-03-24 15:30:56 -0300

    correccion multilenguaje

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,20 +6,24 @@ import English from "../languages/en.json";
 import Portugues from "../languages/br.json";
 import store from "../../redux/store";
 
-const locale = typeof window !== 'undefined' ? navigator.language : 'es-MX';
-let lang;
-
-if (locale === "en") {
-  lang = English;
-}
-if (locale === "br") {
-  lang = Portugues;
-}
-if (locale === "es") {
-  lang = Spanish;
-}
 
 export default function App({ Component, pageProps }) {
+
+  const locale = typeof window !== 'undefined' ? navigator.language : 'es';
+  let lang;
+  
+  if (locale === "en") {
+    lang = English;
+  } else {
+
+    if (locale === "br") {
+      lang = Portugues;
+    } else {
+      lang = Spanish;
+    }
+  }
+
+
   return (
     <IntlProvider locale={locale} messages={lang} >
       <Provider store={store}>
