ffe349443c79fcb80ab2518c4001bcaf611b041a
Neilen <75497135+NeilenC@users.noreply.github.com>
2023-03-24 14:28:09 -0300

    configuracion multilenguaje

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,30 @@
-import '@/styles/globals.css'
-import { Provider } from 'react-redux'
-import store from '../../redux/store'
+import "@/styles/globals.css";
+import { IntlProvider } from "react-intl";
+import { Provider } from "react-redux";
+import Spanish from "../languages/es.json";
+import English from "../languages/en.json";
+import Portugues from "../languages/br.json";
+import store from "../../redux/store";
 
-export default function App({ Component, pageProps }) {
-  return <Provider store={store}>
+const locale = typeof window !== 'undefined' ? navigator.language : 'es-MX';
+let lang;
 
-    <Component {...pageProps} />
-  </Provider> 
+if (locale === "en") {
+  lang = English;
+}
+if (locale === "br") {
+  lang = Portugues;
+}
+if (locale === "es") {
+  lang = Spanish;
 }
 
-
+export default function App({ Component, pageProps }) {
+  return (
+    <IntlProvider locale={locale} messages={lang} >
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </IntlProvider>
+  );
+}
