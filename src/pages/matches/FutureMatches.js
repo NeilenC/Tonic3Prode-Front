8f8a52d66b864ada38e4c9b78802c86bf00bb0d4
Neilen <75497135+NeilenC@users.noreply.github.com>
2023-03-27 17:37:22 -0300

    cambio idiomas

--- src/pages/matches/FutureMatches.js
+++ src/pages/matches/FutureMatches.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { CardContent, Typography } from '@mui/material';
 import styles from "../../styles/matches/FutureMatches.module.css";
 import Grid from './Grid';
+import { FormattedMessage } from "react-intl";
+
 
 const FutureMatches = () => {
   return (
     <CardContent className={styles.global}>
       <div className={styles.title}>
-        <Typography variant="h5">Future Matches</Typography>
+        <Typography variant="h5">
+          <FormattedMessage id="matches"/>
+          </Typography>
       </div>
       <div className={styles['grid-box']}>
         <Grid />
