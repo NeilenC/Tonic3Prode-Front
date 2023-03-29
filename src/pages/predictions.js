470e4aa458b0dd975f1846e9c00ff38bae66312b
Neilen <75497135+NeilenC@users.noreply.github.com>
2023-03-28 21:09:23 -0300

    predicciones

--- src/pages/predictions.js
+++ src/pages/predictions.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Box, TextField } from "@mui/material";
 
 async function getTeams() {
   try {
     const response = await axios.get("http://localhost:3001/api/games");
-    return response.data
+    return response.data;
   } catch (error) {
     return [];
   }
 }
 
-const Predictions = ({ teams = []}) => {
+const Predictions = ({ teams = [] }) => {
   const [scores, setScores] = useState({});
 
   useEffect(() => {
@@ -37,31 +38,50 @@ const Predictions = ({ teams = []}) => {
   if (!teams) return <div>Loading...</div>;
 
   return (
-    <div>
+    <Box
+      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       {teams.map((item) => (
-        <div key={item._id}>
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <span>{item.teams[0].name}</span>
-            <input
-              type="number"
-              value={scores[item._id]?.team1Score || ""}
-              onChange={(e) =>
-                handleScoreChange(item._id, "team1Score", e.target.value)
-              }
-            />
-            <span>-</span>
-            <input
-              type="number"
-              value={scores[item._id]?.team2Score || ""}
-              onChange={(e) =>
-                handleScoreChange(item._id, "team2Score", e.target.value)
-              }
-            />
-            <span>{item.teams[1].name}</span>
-          </div>
-        </div>
+        <Box
+          key={item._id}
+          sx={{ display: "flex", alignItems: "center", my: 2 }}
+        >
+          <img
+            src={item.teams[0].logo_url}
+            alt={item.teams[0].name}
+            style={{ width: "4%" }}
+          />
+          <span style={{ fontSize: "90%", display: "inline-block" }}>
+            {item.teams[0].name}
+          </span>
+          <TextField
+            type="number"
+            value={scores[item._id]?.team1Score || ""}
+            onChange={(e) =>
+              handleScoreChange(item._id, "team1Score", e.target.value)
+            }
+            sx={{ mx: 3, textAlign: "center", width: "10%", height: "1.5%" }}
+          />
+          <span>-</span>
+          <TextField
+            type="number"
+            value={scores[item._id]?.team2Score || ""}
+            onChange={(e) =>
+              handleScoreChange(item._id, "team2Score", e.target.value)
+            }
+            sx={{ mx: 3, textAlign: "center", width: "10%", height: "1.5%" }}
+          />
+          <span style={{ fontSize: "90%", display: "inline-block" }}>
+            {item.teams[1].name}
+          </span>
+          <img
+            src={item.teams[1].logo_url}
+            alt={item.teams[1].name}
+            style={{ width: "4%" }}
+          />
+        </Box>
       ))}
-    </div>
+    </Box>
   );
 };
 
