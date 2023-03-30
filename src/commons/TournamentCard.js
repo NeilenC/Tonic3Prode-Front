e871f3c31f16425c9c068cf59f0d932b33f209b7
MatiasGonzaloDominguez <mat.gon.dom@gmail.com>
2023-03-29 22:29:09 -0300

    add new home components

---
+++ src/commons/TournamentCard.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Card, CardContent, Typography, Divider } from "@mui/material";
+import { format } from "date-fns";
+import { styled } from "@mui/material/styles";
+
+const StyledCard = styled(Card)({
+  display: "flex",
+  flexDirection: "column",
+});
+
+const StyledImgContainer = styled("div")({
+  width: "100%",
+});
+
+const StyledImg = styled("img")({
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+});
+
+const StyledCardContent = styled(CardContent)({
+  flex: 1,
+});
+
+const TournamentCard = ({ tournament }) => {
+  return (
+    <StyledCard
+      sx={{
+        margin: "20px",
+        boxShadow: "0 4px 20px rgba(0,0,0,0.2)",
+        transition: "box-shadow 0.3s ease-in-out",
+        "&:hover": {
+          boxShadow: "0 8px 30px rgba(0,0,0,0.3)",
+        },
+      }}
+    >
+      <StyledImgContainer>
+        <StyledImg
+          src="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blte9c9176021b28a67/635982a86c3afa0bf5601819/FgBCe58WAAADilF.jpg"
+          alt="Tournament"
+        />
+      </StyledImgContainer>
+      <StyledCardContent>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {tournament.title}
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+          {format(new Date(tournament.beginning), "MM/dd/yyyy")} -{" "}
+          {format(new Date(tournament.ending), "MM/dd/yyyy")}{" "}
+          {tournament.stage}nd Round
+        </Typography>
+        <Divider sx={{ my: 2 }} />
+        <Typography variant="h6" gutterBottom>
+          Teams:
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {tournament.teams.length} teams are participating.
+        </Typography>
+      </StyledCardContent>
+    </StyledCard>
+  );
+};
+
+export default TournamentCard;
---
+++ src/pages/home/Tournaments.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import TournamentCard from "@/commons/TournamentCard";
+import { Box, Grid } from "@mui/material";
+
+const Tournaments = ({ width }) => {
+  const [tournaments, setTournaments] = useState([]);
+
+  useEffect(() => {
+    async function searchTournaments() {
+      const response = await axios.get(
+        "http://localhost:3001/api/tournaments/"
+      );
+      return response.data;
+    }
+    searchTournaments().then((data) => setTournaments(data));
+  }, []);
+
+  const isMobile = width === "xs" || width === "sm";
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px",
+      }}
+    >
+      <Box sx={{ maxWidth: "1000px", width: "100%" }}>
+        <Grid container spacing={2} sx={{ display: isMobile ? "column" : "row" }}>
+          {tournaments.map((tournament) => {
+            return (
+              <Grid item key={tournament._id} xs={12} sm={6} md={4}>
+                <TournamentCard tournament={tournament} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+export default Tournaments;
