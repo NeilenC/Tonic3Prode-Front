0a3ff17632e9c640ab4b63ea8e5d80e5e8462e49
MatiasGonzaloDominguez <mat.gon.dom@gmail.com>
2023-03-30 19:33:38 -0300

    new tournament Home

---
+++ src/pages/tournamentHome/[id].js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+import {
+  Scoreboard as MypredictionsIcon,
+  TableRows as FixtureIcon,
+  Stars as RankingIcon,
+  EmojiEvents as PrizesIcon,
+  Sports as TournamentIcon,
+} from "@mui/icons-material";
+import { useRouter } from "next/router";
+
+const Home = ({}) => {
+  const router = useRouter();
+  const id  = router.query.id;
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+      <Button
+        size="xl"
+        variant="outlined"
+        startIcon={<MypredictionsIcon />}
+        sx={{ width: "250px", margin: "25px 0px 15px 0px", fontSize: "20px" }}
+        onClick = {() => {window.location.href = `/tournament/${id}/mypredictions`}}
+      >
+        My predictions
+      </Button>
+
+      <Button
+        size="xl"
+        variant="outlined"
+        startIcon={<FixtureIcon />}
+        sx={{ width: "250px", marginBottom: "15px", fontSize: "20px"}}
+        onClick = {() => {window.location.href = `/tournament/${id}/fixture`}}
+      >
+        Fixture
+      </Button>
+
+      <Button
+        size="xl"
+        variant="outlined"
+        startIcon={<RankingIcon />}
+        sx={{ width: "250px", marginBottom: "15px", fontSize: "20px"}}
+        onClick = {() => {window.location.href = `/tournament/${id}/ranking`}}
+      >
+        Ranking
+      </Button>
+
+      <Button
+        size="xl"
+        variant="outlined"
+        startIcon={<PrizesIcon />}
+        sx={{ width: "250px", marginBottom: "15px", fontSize: "20px"}}
+        onClick = {() => {window.location.href = `/tournament/${id}/prizes`}} 
+      >
+        Prizes
+      </Button>
+
+      <Button
+        size="xl"
+        variant="outlined"
+        startIcon={<TournamentIcon />}
+        sx={{ width: "250px", marginBottom: "15px", fontSize: "20px"}}
+        onClick = {() => {window.location.href = `/home`}}
+      >
+        Tournaments
+      </Button>
+    </Box>
+  );
+};
+
+export default Home;
