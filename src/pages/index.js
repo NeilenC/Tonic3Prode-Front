a6fa02710dcaeee1fee0779aa57af3c2a61a071e
Neilen <75497135+NeilenC@users.noreply.github.com>
2023-04-09 10:45:56 -0300

    condicional signUp

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,27 @@
 import { SignUp } from './logIn/SignUp'
 
 //const inter = Inter({ subsets: ['latin'] })
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import { useEffect } from 'react';
+
+
 
 export default function Home() {
+  const router = useRouter()
+  const userInfo = useSelector((state) => state.userInfo);
+
+  useEffect(() => {
+    if (userInfo) {
+      router.push('/home');
+    }
+  }, [userInfo]);
+
   return (
     <>
-    <SignUp/>
+      {!userInfo ? (
+        <SignUp />
+      ) : null}
     </>
   )
 }
