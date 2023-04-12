68aa7301011b8191a5d5fecce9157e58e64b1b67
jalarg <jlema1990@gmail.com>
2023-04-12 18:21:20 -0300

    countdown terminado finalmente!

--- src/pages/tournamentHome/Predictions/Countdown.js
+++ src/pages/tournamentHome/Predictions/Countdown.js
@@ -1,8 +1,49 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import moment from "moment";
 import Countdown from "react-countdown";
 
-const CustomCountdown = ({ dates, order }) => {
+const CustomCountdown = ({
+  dates,
+  order,
+  setDisableScoreInputs,
+  setTimeRemaining,
+  setTimeUp,
+}) => {
+  const [isTimeUp, setIsTimeUp] = useState(false);
+  const handleTimeUp = () => {
+    setDisableScoreInputs(true);
+    setTimeUp(true);
+  };
+
+  useEffect(() => {
+    if (dates.length && dates[order]) {
+      const eventTimeInMillis = dates[order].getTime();
+      const nowInMillis = new Date().getTime();
+      const timeDiffInMillis = eventTimeInMillis - nowInMillis;
+      setTimeRemaining(timeDiffInMillis);
+    }
+  }, [dates, order]);
+
+  useEffect(() => {
+    const twoHoursInMillis = 2 * 60 * 60 * 1000;
+
+    const interval = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => {
+        const newTimeRemaining = prevTimeRemaining - 1000;
+
+        if (newTimeRemaining <= twoHoursInMillis && !isTimeUp) {
+          handleTimeUp();
+          setIsTimeUp(true); // establecer el estado isTimeUp en true
+        }
+
+        return newTimeRemaining;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [setTimeRemaining, isTimeUp]);
+
   const isValidDate = moment(
     dates[order],
     "ddd MMM DD YYYY HH:mm:ss [GMT]ZZ"
