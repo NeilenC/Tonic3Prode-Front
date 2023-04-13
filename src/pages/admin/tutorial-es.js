91d1e8ba4befea5e7c9a8e99fbd3fad557904c69
Neilen <75497135+NeilenC@users.noreply.github.com>
2023-04-13 19:25:32 -0300

    Carousel re-utilizable para cada tutorial

---
+++ src/pages/admin/tutorial-es.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import TutorialCarousel from "../../commons/TutorialCarousel"
+import puppie1 from "../../../public/puppie1.jpeg";
+import puppie3 from "../../../public/puppie3.jpeg";
+import puppie4 from "../../../public/puppie4.jpeg";
+
+const tutorialAdminEspañol = () => {
+
+  const images = [
+    { id: 1, src: puppie1, alt: "Image 1" },
+    { id: 2, src: puppie3, alt: "Image 2" },
+    { id: 3, src: puppie4, alt: "Image 3" },
+  ];
+
+  return (
+    <div>
+      <TutorialCarousel images={images}/>
+      </div>
+  )
+}
+
+export default tutorialAdminEspañol
