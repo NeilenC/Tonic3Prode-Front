1433a4163cd63bc2cb427bf3cdb07ce7307d9cfd
Neilen <75497135+NeilenC@users.noreply.github.com>
2023-04-13 14:23:14 -0300

    signup

--- src/pages/logIn/SignUp.js
+++ src/pages/logIn/SignUp.js
@@ -1,26 +1,24 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import React, { useEffect } from "react";
-import { signUpGoogle }  from "../../../utils/functions";
+import { signUpGoogle } from "../../../utils/functions";
 import { auth } from "../../../utils/firebaseConfig";
 import GoogleIcon from "@mui/icons-material/Google";
 import { LoginForm } from "./LoginForm";
 import { useSelector, useDispatch } from "react-redux";
-
-
+import { useIntl } from "react-intl";
 
 
 export const SignUp = () => {
-const userInfo = useSelector((state) => state.userInfo);
-const firstLogin = useSelector((state) => state.firstLogin);
-const dispatch = useDispatch();
+  const userInfo = useSelector((state) => state.userInfo);
+  const firstLogin = useSelector((state) => state.firstLogin);
+  const dispatch = useDispatch();
+  const intl = useIntl();
 
-  const handleSignUpGoogle = () => {    
-   signUpGoogle(auth, dispatch);
+  const handleSignUpGoogle = () => {
+    signUpGoogle(auth, dispatch);
   };
 
-  useEffect(() => {
-    
-  }, [firstLogin]);
+  useEffect(() => {}, [firstLogin]);
 
   return (
     <>
@@ -39,7 +37,7 @@ const dispatch = useDispatch();
               }}
             >
               <Typography component="h1" variant="h5" sx={{ mt: 5 }}>
-                Welcome to gambet
+              {intl.formatMessage({ id: "welcome" })}
               </Typography>
               <Box component="form" noValidate sx={{ mt: 1 }}>
                 <Button
@@ -48,7 +46,8 @@ const dispatch = useDispatch();
                   onClick={handleSignUpGoogle}
                   variant="outlined"
                 >
-                  <GoogleIcon /> SignUp with Google
+                  <GoogleIcon />
+                  {intl.formatMessage({ id: "signup" })}
                 </Button>
               </Box>
             </Box>
