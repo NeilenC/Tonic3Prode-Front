2ddd275e77e77fc19f1774dfc580fa3c310f7bc0
MatiasGonzaloDominguez <mat.gon.dom@gmail.com>
2023-04-13 21:51:03 -0300

    Ranking tournament

--- src/pages/admin/tournaments/Ranking.js
+++ src/pages/admin/tournaments/Ranking.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from "next/router";
+import axios from 'axios';
 
 const Ranking = () => {
+  const  [rankings, setRankings] = useState([]);
+  const router = useRouter();
+  const tournamentId = router.query.id;
+  const uid = router.query.uid;
+
+  useEffect(() => {
+    async function getTournamentRankings() {
+      const response = await axios.get(`http://localhost:3001/api/rankings/search/${tournamentId}/${uid}`);
+      setRankings (response.data)
+      }
+    getTournamentRankings();
+  }, []);
+
+  console.log("rankingssssssss", rankings)
+
   return (
     <div>Ranking</div>
   )
