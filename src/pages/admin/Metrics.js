c7ac1a4edc9f1b05fd923bc0839eb0baed5f50ef
MatiasGonzaloDominguez <mat.gon.dom@gmail.com>
2023-04-14 08:27:00 -0300

    New metrics componente, update json, user upgrade with delete and edit

--- src/pages/admin/Metrics.js
+++ src/pages/admin/Metrics.js
@@ -1,15 +1,100 @@
-// ■ Usuarios registrados,
-// ■ Registro de uso de la plataforma por usuario
-// ■ Registro de participantes por etapas del fixture
-// ■ Permanencia en el sistema,
-// ■ Recordatorios enviados.
-
-import React from 'react'
+import React from "react";
+import { Card, Grid, Paper, Typography } from "@mui/material";
+import {
+  BarChart,
+  Bar,
+  PieChart,
+  Pie,
+  Cell,
+  ResponsiveContainer,
+  XAxis,
+} from "recharts";
 
 const Metrics = () => {
-  return (
-    <div>Metrics</div>
-  )
-}
+  const usersCount = 13579; // Cantidad ficticia de usuarios
+  const tournamentsCount = 27; // Cantidad ficticia de torneos
+  const maleUsersCount = 7000; // Cantidad ficticia de usuarios masculinos
+  const femaleUsersCount = 6579; // Cantidad ficticia de usuarios femeninos
+  const basketballTournamentsCount = 10; // Cantidad ficticia de torneos de baloncesto
+  const soccerTournamentsCount = 8; // Cantidad ficticia de torneos de fútbol
+  const volleyballTournamentsCount = 5; // Cantidad ficticia de torneos de voleibol
+  const averageSessionTime = 25; // Tiempo promedio que los usuarios pasan en la plataforma por sesión
 
-export default Metrics
+  const genderData = [
+    { name: "Masculino", value: maleUsersCount },
+    { name: "Femenino", value: femaleUsersCount },
+  ];
+
+  const sportData = [
+    { name: "Baloncesto", value: basketballTournamentsCount },
+    { name: "Fútbol", value: soccerTournamentsCount },
+    { name: "Voleibol", value: volleyballTournamentsCount },
+  ];
+
+  return (
+    <Grid container spacing={2} justify="center">
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx ={{p:2}}>
+          <Typography variant="h6">Usuarios</Typography>
+          <Typography variant="h3">{usersCount}</Typography>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx ={{p:2}}>
+          <Typography variant="h6">Torneos</Typography>
+          <Typography variant="h3">{tournamentsCount}</Typography>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx ={{p:2}}>
+          <Typography variant="h6">Porcentaje de participantes</Typography>
+          <Typography variant="h3">75%</Typography>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6}>
+        <Card sx ={{p:2}}>
+          <Typography variant="h6">
+            Distribución de usuarios por género
+          </Typography>
+          <ResponsiveContainer width="100%" height={300}>
+            <PieChart>
+              <Pie
+                data={genderData}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                innerRadius={60}
+                outerRadius={80}
+              >
+                <Cell fill="#0088FE" />
+                <Cell fill="#00C49F" />
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={6}>
+        <Card sx ={{p:2}}>
+          <Typography variant="h6">
+            Distribución de torneos por deporte
+          </Typography>
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={sportData}>
+              <Bar dataKey="value" fill="#8884d8" />
+              <XAxis dataKey="name" />
+            </BarChart>
+          </ResponsiveContainer>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card sx ={{p:2}}>
+          <Typography variant="h6">Tiempo promedio por sesión</Typography>
+          <Typography variant="h3">{averageSessionTime} min</Typography>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Metrics;
--- src/pages/admin/Users.js
+++ src/pages/admin/Users.js
@@ -9,10 +9,19 @@ import {
   TableHead,
   TableRow,
   Typography,
+  TextField,
+  Button,
+  Modal,
+  Box,
 } from "@mui/material";
 
 const Users = () => {
   const [users, setUsers] = React.useState([]);
+  const [filteredUsers, setFilteredUsers] = React.useState([]);
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const [editingUser, setEditingUser] = React.useState({});
+  const [editedUser, setEditedUser] = React.useState({});
 
   useEffect(() => {
     async function searchUsers() {
@@ -21,51 +30,226 @@ const Users = () => {
         `http://localhost:3001/api/users/${uid}`
       );
       setUsers(response.data);
+      setFilteredUsers(response.data);
     }
     searchUsers();
   }, []);
 
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value;
+    const filteredUsers = users.filter((userData) => {
+      const searchString =
+        userData.username +
+        userData.rol +
+        userData.name +
+        userData.lastName +
+        userData.email +
+        userData.address +
+        userData.country +
+        userData.cellphone +
+        userData.tournaments +
+        userData._id +
+        userData.uid;
+      return searchString.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setFilteredUsers(filteredUsers);
+    setSearchTerm(searchTerm);
+  };
+
+  const handleEditUser = (user) => {
+    setEditingUser(user);
+    setEditedUser(user);
+    setOpen(true);
+  };
+
+  const handleDeleteUser = (user) => {
+    setEditingUser(user);
+    setEditedUser(user);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSaveChanges = () => {
+    // aquí podrías enviar los cambios a la API para que se actualicen en la base de datos
+    // y luego actualizar la tabla localmente
+    const updatedUsers = users.map((user) => {
+      if (user._id === editingUser._id) {
+        return { ...user, ...editedUser };
+      }
+      return user;
+    });
+    setUsers(updatedUsers);
+    setFilteredUsers(updatedUsers);
+    setOpen(false);
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditedUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <Grid container spacing={2} justifyContent="center">
       <Grid item xs={12} md={8}>
+        <TextField
+          label="Search"
+          value={searchTerm}
+          onChange={handleSearch}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
         <TableContainer>
           <Table>
             <TableHead>
               <TableRow>
-                <TableCell>Id</TableCell>
-                <TableCell>Nombre de usuario</TableCell>
-                <TableCell>Correo electrónico</TableCell>
-                <TableCell>UID</TableCell>
-                <TableCell>Nombre</TableCell>
-                <TableCell>Apellido</TableCell>
-                <TableCell>Rol</TableCell>
-                <TableCell>Dirección</TableCell>
-                <TableCell>País</TableCell>
-                <TableCell>Teléfono celular</TableCell>
+                <TableCell>Actions</TableCell>
+                <TableCell>UserName</TableCell>
+                <TableCell>Role</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>LastName</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>address</TableCell>
+                <TableCell>Country</TableCell>
+                <TableCell>Cellphone</TableCell>
                 <TableCell>Torneos</TableCell>
-                <TableCell>__v</TableCell>
+                <TableCell>Id</TableCell>
+                <TableCell>UID</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {users.map((userData) => (
+              {filteredUsers.map((userData) => (
                 <TableRow key={userData._id}>
-                  <TableCell>{userData._id}</TableCell>
+                  <TableCell>
+                    <Button variant="outlined" onClick={() => handleEditUser(userData)}>
+                      Edit
+                    </Button>
+                    <Button variant="outlined" color="error" onClick={() => handleDeleteUser(userData)}>
+                      Delete
+                    </Button>
+                  </TableCell>
                   <TableCell>{userData.username}</TableCell>
-                  <TableCell>{userData.email}</TableCell>
-                  <TableCell>{userData.uid}</TableCell>
+                  <TableCell>{userData.rol}</TableCell>
                   <TableCell>{userData.name}</TableCell>
                   <TableCell>{userData.lastName}</TableCell>
-                  <TableCell>{userData.rol}</TableCell>
+                  <TableCell>{userData.email}</TableCell>
                   <TableCell>{userData.address}</TableCell>
                   <TableCell>{userData.country}</TableCell>
                   <TableCell>{userData.cellphone}</TableCell>
                   <TableCell>{userData.tournaments}</TableCell>
-                  <TableCell>{userData.__v}</TableCell>
+                  <TableCell>{userData._id}</TableCell>
+                  <TableCell>{userData.uid}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
+        <Modal open={open} onClose={handleClose}>
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              bgcolor: "background.paper",
+              boxShadow: 24,
+              p: 4,
+              minWidth: 400,
+            }}
+          >
+            <Typography variant="h6" sx={{ mb: 2 }}>
+              Editar usuario
+            </Typography>
+            <TextField
+              label="UserName"
+              name="username"
+              value={editedUser.username}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              label="Role"
+              name="rol"
+              value={editedUser.rol}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              label="Name"
+              name="name"
+              value={editedUser.name}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              label="LastName"
+              name="lastName"
+              value={editedUser.lastName}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              label="Email"
+              name="email"
+              value={editedUser.email}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              label="Address"
+              name="address"
+              value={editedUser.address}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              label="Country"
+              name="country"
+              value={editedUser.country}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              label="Cellphone"
+              name="cellphone"
+              value={editedUser.cellphone}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              label="Torneos"
+              name="tournaments"
+              value={editedUser.tournaments}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+            />
+            <Button onClick={handleSaveChanges}>Save Changes</Button>
+          </Box>
+        </Modal>
       </Grid>
     </Grid>
   );
