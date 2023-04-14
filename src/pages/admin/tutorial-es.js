b2683c4d88e1bbe6a7d29b3cf13748caf50b0c64
Neilen <75497135+NeilenC@users.noreply.github.com>
2023-04-14 13:44:56 -0300

    tutorial admin

--- src/pages/admin/tutorial-es.js
+++ src/pages/admin/tutorial-es.js
@@ -3,6 +3,7 @@ import TutorialCarousel from "../../commons/TutorialCarousel"
 import puppie1 from "../../../public/puppie1.jpeg";
 import puppie3 from "../../../public/puppie3.jpeg";
 import puppie4 from "../../../public/puppie4.jpeg";
+import { Box, Typography } from '@mui/material';
 
 const tutorialAdminEspañol = () => {
 
@@ -13,9 +14,51 @@ const tutorialAdminEspañol = () => {
   ];
 
   return (
+    <>
     <div>
       <TutorialCarousel images={images}/>
       </div>
+      <Box
+         sx={{
+           backgroundColor: "grey.200",
+           borderRadius: 4,
+           boxShadow: "0px 3px 5px 0px rgba(0,0,0,0.2)",
+           p: 2,
+           mb:4
+         }}
+       >
+         <Typography variant="h6" align="flex-start">
+         Tutorial administrador
+         </Typography> <br />
+         1ª Crear torneos:<br />
+El administrador tiene a disposición la sección de creación de torneos, en la que seleccionará la fecha en la que inicia y culmina el mismo<br />
+Seleccionará los equipos que participarán en el torneo <br />
+Creará los partidos entre equipos<br />
+<br />
+
+
+
+2ª Resultados:<br />
+Teniendo acceso a los resultados del torneo, cargará los mismos en el panel de RESULTS<br />
+Se tendrán en cuenta las etapas en las que se encuentra el torneo <br />
+<br />
+
+
+3ª Premios:<br />
+Tendrá la posibilidad de cargar los premios correspondientes a los futuros ganadores del 1ª, 2ª y 3ª puesto<br />
+Tendrá la responsabilidad de contactarse con los ganadores de los debidos premios al momento siguiente de obtener los resultados una vez culminado dicho torneo <br />
+<br />
+
+
+4ª Edición:<br />
+Puede promoverlos edición de usuarios a administrador<br />
+Puede Eliminar usuarios existentes<br />
+Puede editar equipos<br />
+Puede crear equipos <br />
+Puede editar partidos<br />
+Puede editar resultados de partidos ya jugados en caso de equivocación<br />
+         </Box>
+    </>
   )
 }
 
--- src/pages/user/tutorial-es.js
+++ src/pages/user/tutorial-es.js
@@ -3,6 +3,7 @@ import TutorialCarousel from "../../commons/TutorialCarousel"
 import puppie1 from "../../../public/puppie1.jpeg";
 import puppie3 from "../../../public/puppie3.jpeg";
 import puppie4 from "../../../public/puppie4.jpeg";
+import { Box, Typography } from '@mui/material';
 
 const tutorialUserEspañol = () => {
 
@@ -13,9 +14,58 @@ const tutorialUserEspañol = () => {
   ];
 
   return (
-    <div>
+    <>
+    <div >
       <TutorialCarousel images={images}/>
       </div>
+       <div style={{ maxWidth: "100%", margin: "2%" }}>
+       <Box
+         sx={{
+           backgroundColor: "grey.200",
+           borderRadius: 4,
+           boxShadow: "0px 3px 5px 0px rgba(0,0,0,0.2)",
+           p: 2,
+         }}
+       >
+          <Typography variant="h6" align="flex-start">
+         Tutorial de cómo participar
+         </Typography> <br />
+         1ª Inscríbase al torneo que quiera participar <br />
+         <br/>
+         2ª En esta botonera podrá navegar por las opciones disponibles
+         <br />
+         <br/>
+         3ª Predicciones: <br/>
+         &nbsp; Podrá ver los partidos que se realizaran con sus
+         respectivos equipos, fecha y hora. <br/>
+         &nbsp; Dispondrá de tiempo HASTA DOS HORAS antes de comenzar cada partido.  <br/>&nbsp; ¿¿¿¿Cuando quieras hacer tu predicción, asegúrate de que los resultados tengan números
+         asignados, ya que si alguno de los quipos quedan vacíos, no se
+         guardará la predicción????.
+         <br />
+         <br/>
+         4ª Puntuación: <br/>
+         &nbsp; En caso de haber acertado al ganador, se sumarán 3
+         puntos  <br/>
+         &nbsp; En caso de haber acertado al resultado exacto se sumarán 5
+         puntos  <br/>
+         &nbsp; Tus puntos se verán reflejados luego de obtener el resultado
+         de cada partido <br/>
+         <br/>
+          5ª Ganadores: Los ganadores se dictarán al finalizar
+         el torneo Ganarán aquellos participantes que más puntos obtengan,
+         batiéndose por el 1ª, 2ª y 3ª puesto en relación a los puntos
+         obtenidos  <br/>
+         &nbsp; Si en las posiciones existen dos o mas usuarios empatados
+         en puntos, queda a criterio de cada torneo elegir el desempate  <br/>
+         <br/>
+         6ª Premios: <br/>
+         &nbsp;  Quedan a disposición del/los administrador/es del torneo,
+         quienes se pondrán en contacto una vez obtenidos los resultados.
+         &nbsp; Cada premio se entregará en consecuencia del puesto correspondiente,
+         siendo diferente para cada puesto.
+       </Box>
+     </div>
+     </>
   )
 }
 
