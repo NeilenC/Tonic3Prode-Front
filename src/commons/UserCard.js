c2ab10f3dac6a1c469e42ced580f61a803d6ca55
RosannaContasti <rosicontasti@gmail.com>
2023-04-17 18:43:38 -0300

    Cambios en Ranking User

--- src/commons/UserCard.js
+++ src/commons/UserCard.js
@@ -5,36 +5,25 @@ import Avatar from "@mui/material/Avatar";
 import styles from "@/styles/commons/userCard.module.css";
 
 const UserCard = ({ user }) => {
-  const statusIcon =
-    user.status === "advance" ? (
-      <ArrowUpward style={{ color: "green" }} />
-    ) : user.status === "retrograde" ? (
-      <ArrowDownward style={{ color: "red" }} />
-    ) : (
-      <Remove style={{ color: "gray" }} />
-    );
-
+  
   return (
     <Card className={styles.card}>
       <CardContent className={styles.content}>
-        <div className={styles.cardColumn}>
-          {statusIcon}
-        </div>
         <div className={styles.cardColumn}>
           <p>{user.rank}</p>
         </div>
         <div className={styles.cardColumn}>
           <Avatar
-            alt={user.userName}
+            alt={user.userId.userName}
             src={user.avatarUrl}
             className={styles.avatar}
           />
         </div>
         <div className={styles.cardColumn}>
-          <p>{user.userName}</p>
+          <p>{user.userId.username}</p>
         </div>
         <div className={styles.cardColumn}>
-          <p>{user.points}</p>
+          <p>Score: {user.score}</p>
         </div>
       </CardContent>
     </Card>
